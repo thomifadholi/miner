cf87643519f87a0b8f9b281f10848b1e7b76e04f
thomifadholi <37302546+thomifadholi@users.noreply.github.com>
2021-06-02 16:01:27 +0700

    Add files via upload

---
+++ litecoin.sh
@@ -0,0 +1,7 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz
+tar -zxvf xmrig-6.12.2-linux-x64.tar.gz
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u LTC:ltc1qzrpwcev77pz5ph3yht448xlkdjm8aewje3c48v.LTCSHELL -p x 
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
